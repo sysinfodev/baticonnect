69c49f0667e1adb28e50e608e3bb583652b5ce05
sysinfodev <etudinfogi@gmail.com>
2025-11-21 21:39:45 +0000

    Modification classe responsable
    Added attributes, constructor, getters, setters, and methods to the 'responsable' class.

--- responsale_PAK/responsable.java
+++ responsale_PAK/responsable.java
@@ -1,5 +1,42 @@
 package responsale_PAK;
 
 public class responsable {
-    
+            // les attributs
+    private int cardNumber ;
+    private String cin;
+
+    // Constructeur
+    public responsable(int crd,String ci ){
+        this.cardNumber=crd;
+        this.cin=ci;
+    }  
+    // Getter pour 'cardNumber'
+    public int getCardNumber() {
+        return cardNumber;
+    }
+    // Setter pour 'cardNumber'
+    public void setCardNumber(int crd) {
+        this.cardNumber = crd;
+    } 
+    // Getter pour 'CIN'
+    public String getCIN() {
+        return cin;
+    }
+    // Setter pour 'CIN'
+    public void setCIN(String ci) {
+        this.cin = ci;
+    } 
+    // les methodes
+    public void gererDemandes(){
+    }
+
+    public boolean envoyerRapports(){
+       return false;
+    }
+
+    public void gererRessources(){
+    }
+
+    public void modifierProfil(){
+    }
 }
