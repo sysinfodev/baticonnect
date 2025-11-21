1226f62c842df29c11edd569aef84fcfa45abc07
sysinfodev <etudinfogi@gmail.com>
2025-11-21 21:01:40 +0000

    Implement attributes and methods in responsable class
    Added attributes, constructor, getters, and setters for cardNumber and cin.

--- responsale_PAK/responsable.java
+++ responsale_PAK/responsable.java
@@ -1,5 +1,42 @@
 package responsale_PAK;
 
 public class responsable {
-    
+       // les attributs
+    private int cardNumber ;
+    private String cin;
+
+    // Constructeur
+    public client(int crd;ci ){
+        this.cardNumber=crd;
+        this.cin=ci;
+    }  
+    // Getter pour 'cardNumber'
+    public int getCardNumber() {
+        return cardNumber;
+    }
+    // Setter pour 'cardNumber'
+    public void setCardNumber(int crd) {
+        this.cardNumber = crd;
+    } 
+    // Getter pour 'CIN'
+    public int getCIN() {
+        return cin;
+    }
+    // Setter pour 'CIN'
+    public void setCIN(int ci) {
+        this.cin = ci;
+    } 
+    // les methodes
+    public void gererDemandes(){
+    }
+
+    public boolean envoyerRapports(){
+    return null;
+    }
+
+    public void gererRessources(){
+    }
+
+    public void modifierProfil(){
+    }
 }
